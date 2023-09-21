152b3cbce98e975fd129a45801f6920ba2746ca9
sumit514 <sumitmehta514@gmail.com>
2023-09-22 00:14:24 +0530

    separate useHttp hook

--- custom react hook/src/components/NewTask/NewTask.js
+++ custom react hook/src/components/NewTask/NewTask.js
@@ -1,41 +1,31 @@
-import { useState } from "react";
-
 import Section from "../UI/Section";
 import TaskForm from "./TaskForm";
+import useHttp from "../../hooks/use-http";
 
 const NewTask = (props) => {
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState(null);
+  const transformTaskOnPost = (data, body) => {
+    const generatedId = data.name; // firebase-specific => "name" contains generated id
+    const createdTask = { id: generatedId, text: body.text };
+    props.onAddTask(createdTask);
+  };
 
-  const enterTaskHandler = async (taskText) => {
-    setIsLoading(true);
-    setError(null);
-    try {
-      const response = await fetch(
-        "https://react-custom-http-hook-68481-default-rtdb.firebaseio.com/tasks.json",
-        {
-          method: "POST",
-          body: JSON.stringify({ text: taskText }),
-          headers: {
-            "Content-Type": "application/json",
-          },
-        }
-      );
+  const {
+    isLoading,
+    error,
+    sendRequest: addTaskRequest,
+  } = useHttp(
+    {
+      url: "https://react-custom-http-hook-68481-default-rtdb.firebaseio.com/tasks.json",
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    },
+    transformTaskOnPost
+  );
 
-      if (!response.ok) {
-        throw new Error("Request failed!");
-      }
-
-      const data = await response.json();
-
-      const generatedId = data.name; // firebase-specific => "name" contains generated id
-      const createdTask = { id: generatedId, text: taskText };
-
-      props.onAddTask(createdTask);
-    } catch (err) {
-      setError(err.message || "Something went wrong!");
-    }
-    setIsLoading(false);
+  const enterTaskHandler = (enteredValue1) => {
+    addTaskRequest({ text: enteredValue1 });
   };
 
   return (
--- custom react hook/src/hooks/use-http.js
+++ custom react hook/src/hooks/use-http.js
@@ -4,22 +4,22 @@ const useHttp = (requestConfig, applyData) => {
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
 
-  const sendRequest = async () => {
+  const sendRequest = async (body) => {
     setIsLoading(true);
     setError(null);
     try {
+      console.log("useHttp body : ", requestConfig.body);
       const response = await fetch(requestConfig.url, {
         method: requestConfig.method || "GET",
         header: requestConfig.header || {},
-        body: JSON.stringify(requestConfig.body) || null,
+        body: JSON.stringify(body) || null,
       });
-
       if (!response.ok) {
         throw new Error("Request failed!");
       }
 
       const data = await response.json();
-      applyData(data);
+      applyData(data, body);
     } catch (err) {
       setError(err.message || "Something went wrong!");
     }
