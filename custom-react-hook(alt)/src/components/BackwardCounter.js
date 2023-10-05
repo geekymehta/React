3f0de2a829016e7b017cfbae820fca722b1cb184
sumitmehta021 <136010191+sumitmehta021@users.noreply.github.com>
2023-10-05 16:48:02 +0530

    Add files via upload

---
+++ custom-react-hook(alt)/src/components/BackwardCounter.js
@@ -0,0 +1,18 @@
+import Card from "./Card";
+import useCounter from "../hooks/use-counter";
+
+const BackwardCounter = () => {
+  const counter = useCounter(false);
+
+  // useEffect(() => {
+  //   const interval = setInterval(() => {
+  //     setCounter((prevCounter) => prevCounter - 1);
+  //   }, 1000);
+
+  //   return () => clearInterval(interval);
+  // }, []);
+
+  return <Card>{counter}</Card>;
+};
+
+export default BackwardCounter;
